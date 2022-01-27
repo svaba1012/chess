85cba2a7bcd698d3d622806a4f592d29494b3cfa
svaba1012 <64283829+svaba1012@users.noreply.github.com>
2022-01-27 21:40:47 +0100

    Add files via upload

---
+++ chess/button.py
@@ -0,0 +1,42 @@
+import pygame as pg
+
+def mouseClicked(first):
+    mP = pg.mouse.get_pressed()[0]
+    if mP and first:
+        first = False
+        return True, first
+    elif not mP:
+        first = True
+        return False, first
+    else:
+        return False, False
+
+class button:
+    def __init__(self, x, y, width, height, color, string, fontSize, textC):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.color = color
+        font = pg.font.Font('freesansbold.ttf', fontSize)
+        text = font.render(string, True, textC, self.color)
+        textRect = text.get_rect()
+        textRect.center = (x + width // 2, y + height // 2)
+        self.text = text
+        self.textRect = textRect
+
+    def crtaj(self, surface):
+        pg.draw.rect(surface, self.color, (self.x, self.y, self.width, self.height))
+        surface.blit(self.text, self.textRect)
+        
+    def buttonFunc(self, func):
+        #clicked, prvi = mouseClicked(prvi)
+        clicked = pg.mouse.get_pressed()[0]
+        if clicked:
+            mPos = pg.mouse.get_pos()
+            if mPos[0] > self.x and mPos[0] < self.x + self.width and mPos[1] > self.y and mPos[1] < self.y + self.height:
+                r = func()
+                return r
+        return None
+
+
